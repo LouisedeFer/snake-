ea9996fc81bb5ae1a2d77d9a36afdb491ae44490
Louise de Ferran <louise.deferran@orange.fr>
2024-11-05 16:53:24 +0100

    travail sur la session1"
    q
    quit()

--- snake/main.py
+++ snake/main.py
@@ -1,2 +1,8 @@
+import argparse
+
 def hello() :
-    print('Hello !')
+    parser = argparse.ArgumentParser(description='Some description.')
+    parser.add_argument('-a', help="A text explaining what is the use of the -a option and what type of value it takes.")
+    args = parser.parse_args()
+    
+
