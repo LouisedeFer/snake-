36391b329d551b296b242d252a3a7e15ca20ed84
Louise de Ferran <louise.deferran@orange.fr>
2024-11-07 14:54:14 +0100

    affichage du damier et du serpent

--- snake/background.py
+++ snake/background.py
@@ -2,15 +2,21 @@ import pygame
 # Initialiser le module
 import argparse
 #Sert a donner des instructions
-DEFAULT_HEIGHT=300
-DEFAULT_WIGHT=400
+DEFAULT_LINES=12 #Nombre de lignes
+DEFAULT_COLUMNS=20 #Nombre de colonnes
+DEFAULT_SIZE= 50#Taille du carré
+SNAKE=[3,1]
+
+
 MIN_H=100 ; MAX_H=700
 MIN_W=200 ; MAX_W=800
+
 # lancer le jeu : donner les arguments : poetry run jeu -H valeur -W valeur
 def argu() :
     parser = argparse.ArgumentParser(description='Width and height of the screen')
-    parser.add_argument('-W','--width', type=int, default=DEFAULT_WIGHT,  help="Screen width")
-    parser.add_argument('-H','--height', type=int, default=DEFAULT_HEIGHT, help="Screen height")
+    parser.add_argument('-L','--lines', type=int, default=DEFAULT_LINES,  help="Screen wight (number of columns)")
+    parser.add_argument('-C','--columns', type=int, default=DEFAULT_COLUMNS, help="Screen height (number of lines)")
+    parser.add_argument('-S','--square_size', type=int, default=DEFAULT_SIZE, help="Size of a square")
     args = parser.parse_args()
     #if args.W < MIN_W or args.W>MAX_W:
      #   raise ValueError("The size (-W argument) must be in [200,800]")
@@ -18,12 +24,39 @@ def argu() :
    #    raise ValueError("The size (-H argument) must be in [100,700]")                    
     return args
 
+
+
+
+def tracer_noir(screen,size) :
+    args=argu()
+    color = (0, 0, 0) 
+    for i in range (args.columns) : #on parcourt les colonnes
+        for j in range(args.lines) : #on parcourt les lignes
+            if (i+j)%2==0 : 
+                rect = pygame.Rect(i*size, j*size, size, size)
+                pygame.draw.rect(screen, color, rect)
+     
+
+def tracer_snake(screen, snake, lines, columns, size) : #prend en argument le screen, le tuple de la taille du serpent, sa ligne de depart et sa ligne d'arrivee
+    sna=pygame.Rect(columns*size, lines*size,snake[0]*size, snake[1]*size )
+
+    color=(0,255,0)
+    pygame.draw.rect(screen, color, sna)
+
+    
+
 def jeu() : 
     args=argu()
 
+    size=args.square_size
+    largeur=args.lines * size
+    hauteur=args.columns * size
+    
+
+
     pygame.init()
 
-    screen = pygame.display.set_mode( (400, 300) )
+    screen = pygame.display.set_mode( (hauteur, largeur) )
 
     pygame.display.set_caption("Ecran de jeu")
 
@@ -44,13 +77,20 @@ def jeu() :
                     game=False
 
         
+        
         #bien laisser la boucle d'evnt avant d'afficher
-        screen.fill( (0, 0, 0 ))
+        screen.fill( (255, 255, 255)) #on fait un grand écran blanc où on va rajouter les carrés noirs
+        tracer_noir(screen, size)
+
+        tracer_snake(screen,SNAKE,10,5,size)
+
 
         pygame.display.update() #afficher a la fin
 
 
     pygame.quit()
+
+
 
 
 
